911d70da557235faf825092e34ae42e81ac55311
chakravarthi <chakravarthiviswanath@gmail.com>
2021-07-02 11:54:21 +0530

    Connected

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,24 @@
 import ReactDom from 'react-dom';
 import React,{Component} from 'react';
 import {w3cwebsocket as W3CWebsocket} from "websocket";
+import {Card,Avatar,Input,Typography} from 'antd';
+import 'antd/dist/antd.css';
 
+const {Search} =Input;
 const client=new W3CWebsocket('ws://127.0.0.1:8000');
 
 export default class App extends Component{
+state={
+    userName: '',
+    isLoggedIn:false,
+    messages:[]
+}
+
     onButtonClicked=(value)=>{
         client.send(JSON.stringify({
             type:"message",
-            msg:value
+            msg:value,
+            user:this.state.userName
         }))
     }
     componentDidMount(){
@@ -18,16 +28,48 @@ export default class App extends Component{
         client.onmessage=(message)=>{
             const dataFromServer=JSON.parse(message.data);
             console.log('got reply! ', dataFromServer);
-        }
+            if(dataFromServer.type=="message"){
+                this.setState((state) =>
+                ({
+                  messages: [...state.messages,
+                  {
+                    msg: dataFromServer.msg,
+                    user: dataFromServer.user
+                  }]
+                })
+              );
+            }
+            
+           
+        };
     }
     render(){
         return (
+            <div className="main">
+            
+           
+            {this.state.isLoggedIn?
             <div>
                 <button onClick={()=>this.onButtonClicked("Hello!")}>
                 Send Message
                 </button>
+                {this.state.messages.map(msg=>
+                <p>
+                    message:{msg.msg},user:{msg.user}
+                </p>)}
+            </div>
+                :
+                <div style={{padding:'200px 40px'}}>
+                    <Search
+                    placeholder="Enter Username"
+                    enterButton="Login"
+                    size="large"
+                    onSearch={value => this.setState({ isLoggedIn: true, userName: value })}/>
+                </div>
+                
+            }
             </div>
         );
-    }
-}
+    };
+};
 ReactDom.render(<App/>,document.getElementById('root'));
