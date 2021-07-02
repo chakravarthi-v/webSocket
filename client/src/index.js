95c9af12dd304657978e0cdb65ebdda4f885e594
chakravarthi <chakravarthiviswanath@gmail.com>
2021-07-02 12:13:47 +0530

    changes

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,11 @@ import React,{Component} from 'react';
 import {w3cwebsocket as W3CWebsocket} from "websocket";
 import {Card,Avatar,Input,Typography} from 'antd';
 import 'antd/dist/antd.css';
+import './index.css';
 
 const {Search} =Input;
+const {Text} = Typography;
+const { Meta } = Card;
 const client=new W3CWebsocket('ws://127.0.0.1:8000');
 
 export default class App extends Component{
@@ -19,7 +22,8 @@ state={
             type:"message",
             msg:value,
             user:this.state.userName
-        }))
+        }));
+        this.setState({ searchVal: '' })
     }
     componentDidMount(){
         client.onopen=()=>{
@@ -50,13 +54,34 @@ state={
            
             {this.state.isLoggedIn?
             <div>
-                <button onClick={()=>this.onButtonClicked("Hello!")}>
-                Send Message
-                </button>
-                {this.state.messages.map(msg=>
-                <p>
-                    message:{msg.msg},user:{msg.user}
-                </p>)}
+                <div className="title">
+                <Text id="main-heading" type="secondary" style={{ fontSize: '36px' }}>Websocket: {this.state.userName}</Text>
+
+                </div>
+                <div style={{ display: 'flex', flexDirection: 'column', paddingBottom: 50 }} id="messages">
+            {this.state.messages.map(message => 
+              <Card key={message.msg} style={{ width: 300, margin: '16px 4px 0 4px', alignSelf: this.state.userName === message.user ? 'flex-end' : 'flex-start' }} loading={false}>
+                <Meta
+                  avatar={
+                    <Avatar style={{ color: '#f56a00', backgroundColor: '#fde3cf' }}>{message.user[0].toUpperCase()}</Avatar>
+                  }
+                  title={message.user+":"}
+                  description={message.msg}
+                />
+              </Card> 
+            )}
+          </div>
+               <div className="bottom">
+                <Search
+                    placeholder="Input message"
+                    enterButton="Send"
+                    value={this.state.searchVal}
+                    size="large"
+                    onSearch={value=>this.onButtonClicked(value)}
+                    onChange={(e)=>this.setState({searchVal:e.target.value})}
+                    
+                />
+               </div>
             </div>
                 :
                 <div style={{padding:'200px 40px'}}>
